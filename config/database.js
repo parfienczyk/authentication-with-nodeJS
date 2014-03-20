47a76e62fe161f7184999429d75670b59123a46f
Karol Parfienczyk <parfienczyk@gmail.com>
2014-03-20 15:34:18 +0100

    Config files.

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 // config/database.js
 module.exports = {
 
-	//'url' : 'your_database_here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-	'url' : 'mongodb://karol:karol@alex.mongohq.com:10014/polcode'
+	'url' : 'your_database_here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/kparfienczyk
 
 };
